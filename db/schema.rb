e3227a9455a953e01bd0cab5cd5ad1f5c92160a6
Ryan Batty <rgbatty@outlook.com>
2016-06-07 16:38:29 -0600

    Add images to items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160607174500) do
+ActiveRecord::Schema.define(version: 20160607223403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160607174500) do
     t.string  "name"
     t.string  "description"
     t.integer "price"
+    t.string  "image"
   end
 
 end
