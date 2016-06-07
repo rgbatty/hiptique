3f4c21b3952381f7aa619a2fe372e115ede223c7
Ryan Batty <rgbatty@outlook.com>
2016-06-07 16:33:33 -0600

    Change category to use a slug system

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,18 @@ class Category < ActiveRecord::Base
   has_many :categories_items
   has_many :items, through: :categories_items
 
+  validates_format_of :name, :without => /\A\d/
   validates :name, presence:true, uniqueness: true
+
+  def slug
+    name.parameterize
+  end
+
+  def to_param
+    slug
+  end
+
+  def self.find(input)
+    input.to_i == 0 ? find_by_name(input) : super
+  end
 end
