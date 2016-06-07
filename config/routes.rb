2aa17a6b79451f82d502d579801b9fcb7ecd2cf5
Ilana Corson <ilana.corson@gmail.com>
2016-06-07 13:33:42 -0600

    fully functional for client request 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
 
-  get "/:category_name" => "categories#show"
+  get "/:category_name" => "categories#show", as: "category"
 
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
