93dbfeb3214cfb8133cc0c6bd67340c44752244c
Ilana Corson <ilana.corson@gmail.com>
2016-06-08 10:50:55 -0600

    added some styling to the home page and nav bar

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,11 +1,3 @@
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 end
-#
-# FactoryGirl.define do
-#   factory :user do
-#     username: "default"
-#     password: "password"
-#     admin false
-#   end
-# end
