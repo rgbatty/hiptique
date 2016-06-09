b1de6393b1b32a152c6d0f50e7c4560ea479e225
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-06-09 08:00:03 -0600

    scope in progress for user orders scoping show page for orders under user gives us /user/orders route but we really want just /orders also, we will probably want to preserve the /orders route for all orders so that admin can see all orders

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :index, :create]
   resources :user_carts, only: [:create, :index]
 
-  
-  resources :orders, only: [:index]
+  scope :user do
+    resources :orders, only: [:index]
+  end
 
   get "/cart" => "user_carts#index", as: "cart"
 
