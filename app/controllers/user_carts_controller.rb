b93305d2f8d7fcfde1f3d8727d9de2e96cafbcf9
Ilana Corson <ilana.corson@gmail.com>
2016-06-11 12:24:34 -0600

    in progress cart working to refactor the cart

--- app/controllers/user_carts_controller.rb
+++ app/controllers/user_carts_controller.rb
@@ -4,9 +4,6 @@ class UserCartsController< ApplicationController
     @cart.add_item(item.id)
     session[:cart] = @cart.contents
     redirect_to :back
-    # elsif params[:remove]
-    #   @cart.remove_item(item.id)
-    #   flash[:remove_item] = "Successfully removed #{view_context.link_to item.name, item_path(item)} from your cart."
   end
 
   def new
@@ -16,7 +13,18 @@ class UserCartsController< ApplicationController
     item = Item.find(params[:item_id])
     @cart.update_quantity(item.id, params[:quantity]) if params[:quantity]
     session[:cart] = @cart.contents
+    redirect_to user_carts_path
   end
+
+  def destroy
+    item = Item.find(params[:item_id])
+    
+      if params[:remove]
+      @cart.remove_item(item.id)
+      flash[:remove_item] = "Successfully removed #{view_context.link_to item.name, item_path(item)} from your cart."
+    end
+  end
+
 
   def index
     @cart
