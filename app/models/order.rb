8003552fc4f57239f2f95c0ebebba8fff929d589
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-06-12 15:13:04 -0600

    add ability to create order items added method in orders to create new order items by iterating over the contents in cart

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,13 @@
 class Order < ActiveRecord::Base
   belongs_to :user
+  has_many :order_items
+  has_many :items, through: :order_items
+
+  def create_order_items(cart)
+    cart.obj_contents.each do |content|
+      OrderItem.create(item_id: content.id, order_id: self.id)
+    end
+  end
+
+
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   it {should belong_to(:user)}
+  it {should respond_to(:items)}
+
+  scenario "it can create order items" do
+    order = Order.create
+    item = Item.create(name: "thing", description: "great", price: "10")
+    cart = Cart.new({"#{item.id}" => 1})
+
+    order.create_order_items(cart)
+
+    expect(OrderItem.count).to eq 1
+    expect("thing").to eq(order.items.last.name)
+  end
 end
