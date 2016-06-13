7c9f3619065b4ec28e62e9727161de4d129fdbc8
riizu <rgbatty@outlook.com>
2016-06-13 09:55:49 -0600

    Changes flickering test to use a more specific expect

--- spec/integration/user_can_see_their_past_orders_spec.rb
+++ spec/integration/user_can_see_their_past_orders_spec.rb
@@ -18,10 +18,10 @@ RSpec.feature "user views past orders" do
 
     visit orders_path
 
-    expect(page).to have_content("#{order1.id}")
-    expect(page).to have_content("#{order2.id}")
+    expect(page).to have_content("#{order1.id} #{order1.created_at}")
+    expect(page).to have_content("#{order2.id} #{order2.created_at}")
 
-    expect(page).not_to have_content("#{order3.id}")
-    expect(page).not_to have_content("#{order4.id}")
+    expect(page).not_to have_content("#{order3.id} #{order3.created_at}")
+    expect(page).not_to have_content("#{order4.id} #{order4.created_at}")
   end
 end
