e037d1f6c58b8268cee0541019d5ac965f903be3
riizu <rgbatty@outlook.com>
2016-06-15 22:34:21 -0600

    Removing email, to check and see if it is causing the routing bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.welcome_email(@user).deliver_now
+      #UserMailer.welcome_email(@user).deliver_now
       session[:user_id] = @user.id
       flash[:welcome] = "Thanks for creating an account!"
       redirect_to dashboard_path
